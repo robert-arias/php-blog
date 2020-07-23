25e320b5745d1383bb76eacf59981e2ab9a83d64
robert-arias <robertariascastro@outlook.com>
2020-07-22 22:11:14 -0600

    adding a logging system

--- templates/title.php
+++ templates/title.php
@@ -1,3 +1,11 @@
+<div style="float: right; position: relative; z-index: 99;">
+<?php if (isLoggedIn()): ?>
+        Hello <?php echo htmlEscape(getAuthUser()) ?>.
+        <a href="logout.php">Log out</a>
+    <?php else: ?>
+        <a href="login.php">Log in</a>
+    <?php endif ?>
+</div>
 <div class="header">
     <h1 class="header__title"><a href="index.php" class="header__link">PHP Blog Tutorial</a></h1>
     <p class="header__summary">Blog about making a blog in PHP. The followed tutorial was created by <a href="https://ilovephp.jondh.me.uk/en/tutorial/make-your-own-blog" class="sumarize__link" target="__blank">John</a>.</p>
